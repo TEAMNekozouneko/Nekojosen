589182d5ffd1ffb1d6d8c66cf61f551860b92753
Taitaitatata <70869837+Taitaitatata@users.noreply.github.com>
2023-02-22 22:49:23 +0900

    Update: カウントダウン中と準備時間を追加

--- src/main/java/com/nekozouneko/nekojosen/game/KojosenState.java
+++ src/main/java/com/nekozouneko/nekojosen/game/KojosenState.java
@@ -1,5 +1,23 @@
 package com.nekozouneko.nekojosen.game;
 
 public enum KojosenState {
-    WAITING, PLAYING;
+    WAITING(true),
+    COUNTDOWN(true),
+
+    PREP_TIME(false),
+    PLAYING(false);
+
+    private final boolean waiting;
+
+    KojosenState(boolean waiting) {
+        this.waiting = waiting;
+    }
+
+    public boolean isWaiting() {
+        return waiting;
+    }
+
+    public boolean isPlaying() {
+        return !waiting;
+    }
 }
